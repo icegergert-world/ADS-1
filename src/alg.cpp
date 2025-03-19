1eec4d2cb127e6aec0b377c182971aa702a96860
icegergert-world <andrejkornev26@gmail.com>
2025-03-19 10:29:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,66 +2,61 @@
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-    if (value <= 1) 
-        return false;
-    if (value == 2 || value == 3) 
-        return true;
-    if (value % 2 == 0 || value % 3 == 0)
-        return false;
-
-    for (uint64_t i = 5; i * i <= value; i += 6) {
-        if (value % i == 0 || value % (i + 2) == 0) {
-            return false;
-        }
-    }
+  if (value <= 1)
+    return false;
+  if (value == 2 || value == 3)
     return true;
+  if (value % 2 == 0 || value % 3 == 0)
+    return false;
+
+  for (uint64_t i = 5; i * i <= value; i += 6) {
+    if (value % i == 0 || value % (i + 2) == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    uint64_t Count = 0;
-    uint64_t Number = 2;
+  uint64_t Count = 0;
+  uint64_t Number = 2;
 
-    while (Count < n) {
-        if (checkPrime(Number)) {
-            Count++;
-        }
-        Number++;
+  while (Count < n) {
+    if (checkPrime(Number)) {
+      Count++;
     }
-
-    return Number - 1;
-  return 2;
+    Number++;
+  }
+  return Number - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    if (value <= 1) return 2;
+  if (value <= 1) return 2;
 
-    uint64_t nextnum = value + 1;
-    while (!checkPrime(nextnum)) {
-        nextnum++;
-    }
-    return nextnum;
-  return 2;
+  uint64_t nextnum = value + 1;
+  while (!checkPrime(nextnum)) {
+    nextnum++;
+  }
+  return nextnum;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum = 0;
+  uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; i++) {
-        if (checkPrime(i)) {
-            sum += i;
-        }
+      if (checkPrime(i)) {
+        sum += i;
+      }
     }
     return sum;
-  return 2;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-    uint64_t count = 0;
-    for (uint64_t i = lbound; i < hbound - 2; i++) {
-        if (checkPrime(i) && checkPrime(i + 2)) {
-            count++;
-        }
+  uint64_t count = 0;
+  for (uint64_t i = lbound; i < hbound - 2; i++) {
+    if (checkPrime(i) && checkPrime(i + 2)) {
+      count++;
     }
-    return count;
+  }
+  return count;
 }
